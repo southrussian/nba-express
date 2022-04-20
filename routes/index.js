991502ff84f182826343ce063676590fbff9651a
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-04-20 19:22:06 +0300

    Added Carousel to index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const {Team} = require('../models')
+
 /* GET home page. */
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -14,9 +16,17 @@ router.get('/visitor', function(req, res, next) {
   res.render('visitor', { title: 'Express' });
 });
 
-router.get('/teams', function(req, res, next) {
-  res.render('teams', { title: 'Express' });
+router.get('/players', function(req, res, next) {
+  res.render('players', { title: 'Express' });
 });
+
+router.get('/teams', async function(req, res, next) {
+  let teams = await Team.findAll();
+  console.log(teams)
+  res.render('teams', { title: 'Express', teams});
+
+});
+
 
 
 module.exports = router;
